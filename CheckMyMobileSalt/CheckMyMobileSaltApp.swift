279d7ea5bff74d174c7f14f4105dfcb54381e1d2
Helloyunho <yunho050840@gmail.com>
2021-11-13 17:04:03 +0900

    Initial Commit

---
+++ CheckMyMobileSalt/CheckMyMobileSaltApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMyMobileSaltApp.swift
+//  CheckMyMobileSalt
+//
+//  Created by Helloyunho on 2021/11/13.
+//
+
+import SwiftUI
+
+@main
+struct CheckMyMobileSaltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
